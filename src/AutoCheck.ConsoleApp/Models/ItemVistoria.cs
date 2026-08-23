d4c4684ef8f0b14f666acc87853b50cb49276df0
Joao Vitor da Nova <danovajoaovitor@gmail.com>
2026-08-23 19:20:57 -0300

    Implementa validaçao do status e pontuacao do ItemVistoria

--- src/AutoCheck.ConsoleApp/Models/ItemVistoria.cs
+++ src/AutoCheck.ConsoleApp/Models/ItemVistoria.cs
@@ -1,9 +1,37 @@
 namespace AutoCheck.ConsoleApp.Models
 {
-    public class ItemVistoria
+  public class ItemVistoria
   {
     public string Nome { get; set; }
     public string Status { get; set; }
-  }
 
+    public ItemVistoria(string nome, string status)
+    {
+      if (status.ToLower() != "bom" && status.ToLower() != "regular" && status.ToLower() != "ruim")
+      {
+        Console.WriteLine("Status inválido. Utilize apenas: 'Bom', 'Regular' ou 'Ruim'.");
+      }
+      Nome = nome;
+      Status = status;
+    }
+
+    public int ObterPontuacao()
+    {
+      int pontuacao = 0;
+
+      if (Status.ToLower() == "bom")
+      {
+        pontuacao = 10;
+      }
+      else if (Status.ToLower() == "regular")
+      {
+        pontuacao = 5;
+      }
+      else if (Status.ToLower() == "ruim")
+      {
+        pontuacao = 0;
+      }
+      return pontuacao;
+    }
+  }
 }
